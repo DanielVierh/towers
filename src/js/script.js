3f43ed70eee5f753fd82c36b840fabbdd780c8f9
DanielVierh <daniel.vierheilig89@yahoo.de>
2025-04-12 14:07:01 +0200

    old map

--- src/js/script.js
+++ src/js/script.js
@@ -52,28 +52,28 @@ let waypoints = [
 ];
 
 // * Level 2
-waypoints = [
-  { x: -50, y: 80 },
-  { x: 50, y: 80 },
-  { x: 110, y: 80 },
-  { x: 110, y: 0 },
-  { x: 270, y: 0 },
-  { x: 270, y: 110 },
-  { x: 140, y: 110 },
-  { x: 140, y: 190 },
-  { x: 360, y: 190 },
-  { x: 360, y: 250 },
-  { x: 110, y: 250 },
-  { x: 110, y: 330 },
-  { x: 450, y: 330 },
-];
+// waypoints = [
+//   { x: -50, y: 80 },
+//   { x: 50, y: 80 },
+//   { x: 110, y: 80 },
+//   { x: 110, y: 0 },
+//   { x: 270, y: 0 },
+//   { x: 270, y: 110 },
+//   { x: 140, y: 110 },
+//   { x: 140, y: 190 },
+//   { x: 360, y: 190 },
+//   { x: 360, y: 250 },
+//   { x: 110, y: 250 },
+//   { x: 110, y: 330 },
+//   { x: 450, y: 330 },
+// ];
 
 const enemies = [];
 const lasers = [];
 const moneyPopups = [];
 const backgroundImage = new Image();
 backgroundImage.src = "src/assets/bg/bg2.webp";
-backgroundImage.src = "src/assets/bg/desert_bg.png";
+// backgroundImage.src = "src/assets/bg/desert_bg.png";
 let waveTimer = 10; // Timer für die nächste Welle in Sekunden
 let tower = undefined;
 let show_tower_range = false;
