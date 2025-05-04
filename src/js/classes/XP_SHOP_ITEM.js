bfea0499590eab9adf503319914472c94c000566
DanielVierh <daniel.vierheilig89@yahoo.de>
2025-05-04 22:22:23 +0200

    add xp show item class

---
+++ src/js/classes/XP_SHOP_ITEM.js
@@ -0,0 +1,8 @@
+export class XP_SHOP_ITEM {
+    constructor(name, xpc, values, description) {
+        this.name = name;
+        this.xpc = xpc;
+        this.values = values;
+        this.description = description;
+    }
+}
