f0c73904bae5f781cad16783feebec696b5bbb48
DanielVierh <daniel.vierheilig89@yahoo.de>
2025-09-15 21:18:23 +0200

    styles for bought item

--- src/js/functions/xp_Items.js
+++ src/js/functions/xp_Items.js
@@ -1,20 +1,25 @@
-
-
-//* Display Amount of Items 
+//* Display Amount of Items
 export function render_amount(save_obj) {
-  const lbl_trap_discount_amount = document.getElementById("lbl_trap_discount_amount");
-  const lbl_mine_discount_amount = document.getElementById("lbl_mine_discount_amount");
-  const lbl_tower_discount_amount = document.getElementById("lbl_tower_discount_amount");
-  const lbl_buymenu_tower_discount_amount = document.getElementById("lbl_buymenu_tower_discount_amount");
-  const checkbox_field_tower = document.getElementById('checkbox_field_tower');
-  const checkbox_field_trap = document.getElementById('checkbox_field_trap');
-
-
+  const lbl_trap_discount_amount = document.getElementById(
+    "lbl_trap_discount_amount"
+  );
+  const lbl_mine_discount_amount = document.getElementById(
+    "lbl_mine_discount_amount"
+  );
+  const lbl_tower_discount_amount = document.getElementById(
+    "lbl_tower_discount_amount"
+  );
+  const lbl_buymenu_tower_discount_amount = document.getElementById(
+    "lbl_buymenu_tower_discount_amount"
+  );
+  const checkbox_field_tower = document.getElementById("checkbox_field_tower");
+  const checkbox_field_trap = document.getElementById("checkbox_field_trap");
+  const tile_live_upgr = document.getElementById("tile_live_upgr");
 
   let use_discount = {
     trap_discount: false,
-    tower_discount: false
-  }
+    tower_discount: false,
+  };
 
   //* Show Amount of Trap Discount
   const item = return_Item_Amount_and_existence(save_obj, "trap_rabatt_50");
@@ -22,36 +27,46 @@ export function render_amount(save_obj) {
     lbl_trap_discount_amount.innerHTML = `${item.amount}X`;
     lbl_mine_discount_amount.innerHTML = `${item.amount} X 50% Rabatt`;
 
-    if(item.amount > 0) {
-      checkbox_field_trap.style.display = 'block';
-    }else {
-      checkbox_field_trap.style.display = 'none';
+    if (item.amount > 0) {
+      checkbox_field_trap.style.display = "block";
+    } else {
+      checkbox_field_trap.style.display = "none";
     }
   }
 
   //* Show Amount of Tower Discount
-  const tower_item = return_Item_Amount_and_existence(save_obj, 'tower_rabatt_50');
-  if(tower_item.available) {
+  const tower_item = return_Item_Amount_and_existence(
+    save_obj,
+    "tower_rabatt_50"
+  );
+  if (tower_item.available) {
     lbl_tower_discount_amount.innerHTML = `${tower_item.amount}X`;
     lbl_buymenu_tower_discount_amount.innerHTML = `${tower_item.amount} X 50% Rabatt`;
+  }
+
+  const live_upgr = return_Item_Amount_and_existence(
+    save_obj,
+    "live_generator"
+  );
+  if (live_upgr) {
+    tile_live_upgr.classList.add("is-active");
   }
 }
 
 export function render_XP_Coins(save_obj) {
   const lbl_xp_store_coins = document.getElementById("lbl_xp_store_coins");
   lbl_xp_store_coins.innerHTML = `${save_obj.XP_Coins.toLocaleString(
-      "de-DE"
-    )} XP-Coins`;
+    "de-DE"
+  )} XP-Coins`;
 }
-
 
 export function return_Item_Amount_and_existence(save_obj, name) {
   let return_obj = {
-        available: false,
-        amount: 0,
-        index: -1
-      };
-  
+    available: false,
+    amount: 0,
+    index: -1,
+  };
+
   for (let i = 0; i < save_obj.XP_Store_Items.length; i++) {
     if (save_obj.XP_Store_Items[i].name === name) {
       return_obj.available = true;
